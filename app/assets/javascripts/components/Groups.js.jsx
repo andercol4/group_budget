f0afec798060c3366847572ef77202e41d5359f4
Colin Anderson <andercol.4@gmail.com>
2016-01-22 14:49:18 -0700

    group create function working

--- app/assets/javascripts/components/Groups.js.jsx
+++ app/assets/javascripts/components/Groups.js.jsx
@@ -1,7 +1,42 @@
 class Groups extends React.Component{
   constructor(props){
     super(props);
-    this.state = {groups: this.props.groups}
+    this.toggleGroupForm = this.toggleGroupForm.bind(this);
+    this.groupForm = this.groupForm.bind(this);
+    this.submitNewGroup = this.submitNewGroup.bind(this);
+    this.state = {groups: this.props.groups, groupForm: false}
+  }
+  toggleGroupForm(){
+    this.setState({groupForm: !this.state.groupForm})
+  }
+  groupForm(){
+    if(this.state.groupForm){
+      // debugger
+      return(
+        <div>
+          <form  onSubmit={this.submitNewGroup}>
+            <div className="form-group">
+              <label>Group Name</label>
+              <input type='text' ref='groupName' placeholder='Group Name'></input>
+            </div>
+            <button type='submit'>Submit</button>
+          </form>
+        </div>);
+    }
+  }
+  submitNewGroup(e){
+    // debugger
+    e.preventDefault()
+    $.ajax({
+      url: '/groups',
+      type: 'POST',
+      data: {group: {name: this.refs.groupName.value}}
+    }).success(data => {
+      let groups = this.state.groups
+      groups.push(data)
+      this.setState({groups})
+    }).error( data => {
+    });
   }
   render(){
     let groups = this.state.groups.map(group => {
@@ -9,7 +44,9 @@ class Groups extends React.Component{
     })
     return(
       <div>
-      {groups}
+        <button onClick={this.toggleGroupForm}>New Group</button>
+        {this.groupForm()}
+        {groups}
       </div>)
   }
 
