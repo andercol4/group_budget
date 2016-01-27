b465abceeb227cac39e038d23425cf2e75a56028
Colin Anderson <andercol.4@gmail.com>
2016-01-27 12:05:10 -0700

    creates a personal group on user sign up

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,7 +1,19 @@
 class RegistrationsController < Devise::RegistrationsController
+  after_filter :add_personal
+
+
   private
   def sign_up_params
     params.require(:user).permit(:first_name, :last_name, :username, :email, :password, :password_confirmation)
+  end
+
+  def add_personal
+    if resource.persisted? # user is created successfuly
+      group = Group.create(name: "#{resource.first_name} Personal", creator_id: resource.id )
+      user_group = UserGroup.create(user_id: resource.id, group_id: group.id)
+      binding.pry
+
+    end
   end
 
   def account_update_params
