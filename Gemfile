cdcf0e579555f3d4704aff1ae943b78e30977fab
Beardog <beardog@gmail.com>
2016-02-01 10:02:10 -0700

    landing,faq,about

--- Gemfile
+++ Gemfile
@@ -22,6 +22,7 @@ gem 'jbuilder', '~> 2.0'
 gem 'sdoc', '~> 0.4.0', group: :doc
 gem 'haml-rails', '~> 0.9'
 gem 'bootstrap-sass'
+gem 'fontawesome-rails'
 gem 'devise', '~> 3.5'
 gem 'bourbon'
 gem 'chart-js-rails'
--- app/assets/javascripts/dashboard.js
+++ app/assets/javascripts/dashboard.js
@@ -1,36 +1,36 @@
-$(function() {
-	var data = {
-    labels: ["Group1", "March", "April"],
-    datasets: [
-        {
-            label: "My First dataset",
-            fillColor: "rgba(220,220,220,0.5)",
-            strokeColor: "rgba(220,220,220,0.8)",
-            highlightFill: "rgba(220,220,220,0.75)",
-            highlightStroke: "rgba(220,220,220,1)",
-            data: [65, 35, 45]
-        }
-        // {
-        //     label: "My Second dataset",
-        //     fillColor: "rgba(151,187,205,0.5)",
-        //     strokeColor: "rgba(151,187,205,0.8)",
-        //     highlightFill: "rgba(151,187,205,0.75)",
-        //     highlightStroke: "rgba(151,187,205,1)",
-        //     data: [28]
-        // },
-        // {
-        //     label: "My Second dataset",
-        //     fillColor: "rgba(151,187,205,0.5)",
-        //     strokeColor: "rgba(151,187,205,0.8)",
-        //     highlightFill: "rgba(151,187,205,0.75)",
-        //     highlightStroke: "rgba(151,187,205,1)",
-        //     data: [28]
-        // }
-    ]
-	};	
-	var ctx = document.getElementById("groupChart").getContext("2d");
-	var myBarChart = new Chart(ctx).Bar(data, {responsive: true});
-});
+// $(function() {
+// 	var data = {
+//     labels: ["Group1", "March", "April"],
+//     datasets: [
+//         {
+//             label: "My First dataset",
+//             fillColor: "rgba(220,220,220,0.5)",
+//             strokeColor: "rgba(220,220,220,0.8)",
+//             highlightFill: "rgba(220,220,220,0.75)",
+//             highlightStroke: "rgba(220,220,220,1)",
+//             data: [65, 35, 45]
+//         }
+//         // {
+//         //     label: "My Second dataset",
+//         //     fillColor: "rgba(151,187,205,0.5)",
+//         //     strokeColor: "rgba(151,187,205,0.8)",
+//         //     highlightFill: "rgba(151,187,205,0.75)",
+//         //     highlightStroke: "rgba(151,187,205,1)",
+//         //     data: [28]
+//         // },
+//         // {
+//         //     label: "My Second dataset",
+//         //     fillColor: "rgba(151,187,205,0.5)",
+//         //     strokeColor: "rgba(151,187,205,0.8)",
+//         //     highlightFill: "rgba(151,187,205,0.75)",
+//         //     highlightStroke: "rgba(151,187,205,1)",
+//         //     data: [28]
+//         // }
+//     ]
+// 	};	
+// 	var ctx = document.getElementById("groupChart").getContext("2d");
+// 	var myBarChart = new Chart(ctx).Bar(data, {responsive: true});
+// });
 
 $(document).ready(function() {
   var origheight = $("#trans1").height();
