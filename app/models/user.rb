9a1836915f2c90039c0537f4b4d263d946628c5c
Seth E <savethewhales77@gmail.com>
2016-02-02 15:58:52 -0700

    fixex omniauth bug

--- app/models/user.rb
+++ app/models/user.rb
@@ -36,6 +36,9 @@ class User < ActiveRecord::Base
       where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
         user.email = auth.info.email
         user.password = Devise.friendly_token[0,20]
+        user.username = auth.info.email
+        user.first_name = auth.info.name.split(" ").first
+        user.last_name = auth.info.name.split(" ").last
       end
   end
 
