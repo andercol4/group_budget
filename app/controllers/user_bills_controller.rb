e9e20de18526ec6e5ea6a6d3dada76e436998f53
Colin Anderson <andercol.4@gmail.com>
2016-02-03 12:13:56 -0700

    100% user_bill_controller coverage

--- app/controllers/user_bills_controller.rb
+++ app/controllers/user_bills_controller.rb
@@ -1,40 +1,39 @@
 class UserBillsController < ApplicationController
   before_action :authenticate_user!
   def pay_bill
-      @user_bill = UserBill.find_by(bill_id: params[:id], user_id: current_user.id)
-      @bill = Bill.find(params[:id])
-      if @user_bill
-        @user_bill.amount_paid += params[:user_bill][:amount_paid].to_f
-        @user_bill.amount_owed -= params[:user_bill][:amount_paid].to_f
-        if @user_bill.amount_owed <= 0.01
-          @user_bill.is_paid = true
-        end
-        @bill.amount_paid += params[:user_bill][:amount_paid].to_f
-        if  @bill.amount_paid >= @bill.amount_total - 0.02
-          @bill.is_paid = true
-          if @bill.recurring == true && params[:user_bill][:amount_paid].to_f > 0.01
-            @group = Group.find(@bill.group_id)
-            @new_bill = @group.bills.new
-            @new_bill.name = @bill.name
-            # binding.pry
-            @new_bill.due_date = @bill.due_date + 1.month
-            @new_bill.recurring = @bill.recurring
-            @new_bill.amount_total = @bill.amount_paid
-            @new_bill.creator_id = @bill.creator_id
-            @new_bill.save
-            @new_bill.divide_bill
-
-          end
-        end
-        @bill.save
-        @user_bill.save
-        render json: {user_bill: @user_bill, bill: @bill}
-      else
-          #inform user that there is nothing to be paid
+    @user_bill = UserBill.find_by(bill_id: params[:id], user_id: current_user.id)
+    @bill = Bill.find(params[:id])
+    if @user_bill
+      @user_bill.amount_paid += params[:user_bill][:amount_paid].to_f
+      @user_bill.amount_owed -= params[:user_bill][:amount_paid].to_f
+      if @user_bill.amount_owed <= 0.01
+        @user_bill.is_paid = true
       end
+      @bill.amount_paid += params[:user_bill][:amount_paid].to_f
+      if  @bill.amount_paid >= @bill.amount_total - 0.02
+        @bill.is_paid = true
+        if @bill.recurring == true && params[:user_bill][:amount_paid].to_f > 0.01
+          @group = Group.find(@bill.group_id)
+          @new_bill = @group.bills.new
+          @new_bill.name = @bill.name
+          # binding.pry
+          @new_bill.due_date = @bill.due_date + 1.month
+          @new_bill.recurring = @bill.recurring
+          @new_bill.amount_total = @bill.amount_paid
+          @new_bill.creator_id = @bill.creator_id
+          @new_bill.save
+          @new_bill.divide_bill
+        end
+      end
+      @bill.save
+      @user_bill.save
+      if @new_bill
+        render json: {user_bill: @user_bill, bill: @bill, new_bill: @new_bill}
+      else
+        render json: {user_bill: @user_bill, bill: @bill}
+      end
+    else
+        #inform user that there is nothing to be paid
     end
-  private
-  def user_bill_params
-    params.require(:user_bill).permit(:amount_owed, :amount_paid, :is_paid)
   end
 end
--- spec/controllers/user_bills_controller_spec.rb
+++ spec/controllers/user_bills_controller_spec.rb
@@ -2,4 +2,69 @@ require 'rails_helper'
 
 RSpec.describe UserBillsController, type: :controller do
 
+  describe 'pay_bill method' do
+    let(:user1){User.create(username: "User1",
+      email:"user1@email.com",
+      first_name: "User1",
+      password: "password",
+      password_confirmation: 'password',
+      last_name: 'Hancock')}
+    let(:user2){User.create(username: "User2",
+      email:"user2@email.com",
+      first_name: "User2",
+      password: "password",
+      password_confirmation: 'password',
+      last_name: 'Hancock')}
+    let(:group){Group.create(name: "House", creator_id: user1.id)}
+    let(:ug1){UserGroup.create(user_id: user1.id, group_id: group.id)}
+    let(:ug2){UserGroup.create(user_id: user2.id, group_id: group.id)}
+    let(:bill){Bill.create(name: "Rent", due_date: 5.day.from_now, group_id: group.id, amount_total: 100, creator_id: user1.id)}
+    let(:ub1){UserBill.create(user_id: user1.id, bill_id: bill.id, amount_owed: 50)}
+    let(:ub2){UserBill.create(user_id: user2.id, bill_id: bill.id, amount_owed: 50)}
+
+
+    before(:each) do
+      controller.class.skip_before_filter :authenticate_user!
+      sign_in user1
+    end
+
+    it 'marks userbills paid accordingly' do
+      user1
+      group
+      ug1
+      bill
+      ub1
+      put :pay_bill, {id: bill.id, user_bill: {amount_paid: 50}}
+      # binding.pry
+      body = JSON.parse(response.body)
+      expect(response).to be_success
+      expect(body['user_bill']['is_paid']).to eq(true)
+    end
+    it 'marks bill as paid when it is paid in full' do
+      user1
+      group
+      ug1
+      bill_single = Bill.create(name: "Rent", due_date: 5.day.from_now, group_id: group.id, amount_total: 50, creator_id: user1.id)
+      ub = UserBill.create(user_id: user1.id, bill_id: bill_single.id, amount_owed: 50)
+      # binding.pry
+      put :pay_bill, {id: bill_single.id, user_bill: {amount_paid: 50}}
+      expect(response).to be_success
+      body = JSON.parse(response.body)
+      expect(body['bill']['is_paid']).to eq(true)
+    end
+    it 'makes a new bill if recurring is true' do
+      user1
+      group
+      ug1
+      bill_single = Bill.create(name: "Rent", due_date: 5.day.from_now, group_id: group.id, amount_total: 50, creator_id: user1.id, recurring: true)
+      ub = UserBill.create(user_id: user1.id, bill_id: bill_single.id, amount_owed: 50)
+      # binding.pry
+      put :pay_bill, {id: bill_single.id, user_bill: {amount_paid: 50}}
+      expect(response).to be_success
+      body = JSON.parse(response.body)
+      expect(body['new_bill']['is_paid']).to eq(false)
+      expect(body['new_bill']['name']).to eq('Rent')
+    end
+  end
+
 end
