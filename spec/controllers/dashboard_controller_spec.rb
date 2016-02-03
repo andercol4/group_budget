5beefb42774079e8872e84a8155929484e67a9f5
Kaitlin Anderson <kaitlin_anderson@mac.com>
2016-02-03 15:01:29 -0700

    dashboard test, not done

--- spec/controllers/dashboard_controller_spec.rb
+++ spec/controllers/dashboard_controller_spec.rb
@@ -1,11 +1,34 @@
 require 'rails_helper'
 
 RSpec.describe DashboardController, type: :controller do
+  let(:user) {User.create(username: "UserH",
+    email:"user@email.com",
+    first_name: "User",
+    password: "password",
+    password_confirmation: 'password',
+    last_name: 'Hancock')}
+  let(:group) {Group.create(name: "UserH Personal", creator_id: user.id)}
+  let(:ug) {UserGroup.create(user_id: user.id, group_id: group.id)}
+  let(:comment) {Comment.create(body: "Hello", group_id: group.id, user_id: user.id)}
+
+  # before(:each) do
+  #   controller.class.skip_before_filter :authenticate_user!
+  #   sign_in user
+  # end
 
   describe "GET #index" do
     it "returns http success" do
       get :index
       expect(response).to have_http_status(:success)
+    end
+
+    it "sets the groups instance variable" do
+      controller.class.skip_before_filter :authenticate_user!
+      sign_in user
+      group
+      ug
+      get :index, current_user: user.groups
+      expect(assigns(:groups)).to eq(user.groups)
     end
   end
 
